bfb7076f9f21e3eb5a1f9d6197ef12db27c35eb7
Alex Kazakov <kazakov_ab@mail.ru>
2018-06-30 18:43:59 +0200

    Building workers and pylons

--- bot/main.py
+++ bot/main.py
@@ -5,15 +5,45 @@ import sc2
 
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 class MyBot(sc2.BotAI):
     with open(Path(__file__).parent / "../botinfo.json") as f:
         NAME = json.load(f)["name"]
 
+
     async def on_step(self, iteration):
         if iteration == 0:
             await self.chat_send(f"Name: {self.NAME}")
 
-        # what to do every step
-        await self.distribute_workers()  # in sc2/bot_ai.py
+        await self.distribute_workers()  # in sc2/bot_ai.p
+        await self.build_workers()
+        await self.build_assimilator()
+        await self.build_pylons()
+
+
+    async def build_workers(self):
+        for nexus in self.units(NEXUS).ready.noqueue:
+            if self.can_afford(PROBE):
+                await self.do(nexus.train(PROBE))
+
+    async def build_pylons(self):
+        if self.supply_left < 5 and not self.already_pending(PYLON):
+            nexuses = self.units(NEXUS).ready
+            if nexuses.exists:
+                if self.can_afford(PYLON):
+                    await self.build(PYLON, near=nexuses.first)
+
+
+    async def build_assimilator(self):
+        for nexus in self.units(NEXUS).ready:
+            print("n")
+            vaspenes = self.state.vespene_geyser.closer_than(10.0, nexus)
+            print("z")
+            for vaspene in vaspenes:
+                print("a")
+                if (not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists) and self.can_afford(ASSIMILATOR):
+                    print("b")
+                    worker = self.select_build_worker(vaspene.position)
+                    if not (worker is None):
+                        await self.do(worker.build(ASSIMILATOR, vaspene))
