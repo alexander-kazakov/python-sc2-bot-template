3d1eac3b346f71ad1e63cc9ba60333c47b4ef256
Alex Kazakov <kazakov_ab@mail.ru>
2018-06-30 18:05:49 +0200

    The bot can run!

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,10 @@ from pathlib import Path
 
 import sc2
 
+from sc2 import run_game, maps, Race, Difficulty
+from sc2.player import Bot, Computer
+
+
 class MyBot(sc2.BotAI):
     with open(Path(__file__).parent / "../botinfo.json") as f:
         NAME = json.load(f)["name"]
@@ -10,3 +14,6 @@ class MyBot(sc2.BotAI):
     async def on_step(self, iteration):
         if iteration == 0:
             await self.chat_send(f"Name: {self.NAME}")
+
+        # what to do every step
+        await self.distribute_workers()  # in sc2/bot_ai.py
--- run_locally.py
+++ run_locally.py
@@ -1,5 +1,6 @@
 import json
 
+import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 
@@ -11,9 +12,9 @@ def main():
 
     race = Race[info["race"]]
 
-    run_game(maps.get("Abyssal Reef LE"), [
+    run_game(maps.get("(2)DreamcatcherLE"), [
         Bot(race, MyBot()),
-        Computer(Race.Random, Difficulty.Medium)
+        Computer(Race.Random, Difficulty.Easy)
     ], realtime=False, step_time_limit=2.0, game_time_limit=(60*20), save_replay_as="test.SC2Replay")
 
 if __name__ == '__main__':
