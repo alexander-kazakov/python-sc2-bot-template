98e60ae1a7018e279c346aea5e89d0cb91831133
Alex Kazakov <kazakov_ab@mail.ru>
2018-07-21 21:21:57 +0200

    Builds SCVs and Supply Depots

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,12 @@
-from .helpers import load_bot_name
+import sys
+sys.path.insert(0, '/Users/alexanderkazakov/Documents/MyCode/sc2-mailmanich-test-bot/bot/')
+from helpers import load_bot_name
 
 import sc2
 
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
+from sc2.constants import COMMANDCENTER, SCV, SUPPLYDEPOT, ASSIMILATOR
 
 
 class MyBot(sc2.BotAI):
@@ -16,40 +18,40 @@ class MyBot(sc2.BotAI):
 
         await self.distribute_workers()  # in sc2/bot_ai.p
 
-    #     await self.build_workers()
-    #
-    # async def build_workers(self):
-    #     for nexus in self.units(NEXUS).ready.noqueue:
-    #         if self.can_afford(probe):
-    #             await self.do(nexus.train(PROBE))
-    #
-    # async def build_pylons(self):
-    #     if self.supply_left < 5 and not self.already_pending(PYLON):
-    #         nexuses = self.units(NEXUS).ready
-    #         if nexuses.exists:
-    #             if self.can_afford(PYLON):
-    #                 await self.build(PYLON, near=nexuses.first)
-    #
-    #
-    # async def build_assimilator(self):
-    #     for nexus in self.units(NEXUS).ready:
-    #         vaspenes = self.state.vespene_geyser.closer_than(10.0, nexus)
-    #         for vaspene in vaspenes:
-    #             if (not self.units(ASSIMILATOR).closer_than(1.0, vaspene)
-    #                               .exists) and self.can_afford(ASSIMILATOR):
-    #                 worker = self.select_build_worker(vaspene.position)
-    #                 if not (worker is None):
-    #                     await self.do(worker.build(ASSIMILATOR, vaspene))
-    #
-    # async def attack(self, iteration):
-    #     self.log_info(iteration, "Attacking!")
-    #     if (self.known_enemy_units.amount > 0):
-    #         for s in self.units(PROBE):
-    #             await self.do(s.attack(self.known_enemy_units.random))
-    #
-    # async def expand(self):
-    #     if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
-    #         await self.expand_now()
+        await self.build_workers()
+        await self.build_supply_depots()
+
+    async def build_workers(self):
+        for command_center in self.units(COMMANDCENTER).ready.noqueue:
+            if self.can_afford(SCV):
+                await self.do(command_center.train(SCV))
+
+    async def build_supply_depots(self):
+        if self.supply_left < 5 and not self.already_pending(SUPPLYDEPOT):
+            command_centers = self.units(COMMANDCENTER).ready
+            if command_centers.exists:
+                if self.can_afford(SUPPLYDEPOT):
+                    await self.build(SUPPLYDEPOT, near=command_centers.first)
+
+# async def build_assimilator(self):
+#     for nexus in self.units(NEXUS).ready:
+#         vaspenes = self.state.vespene_geyser.closer_than(10.0, nexus)
+#         for vaspene in vaspenes:
+#             if (not self.units(ASSIMILATOR).closer_than(1.0, vaspene)
+#                               .exists) and self.can_afford(ASSIMILATOR):
+#                 worker = self.select_build_worker(vaspene.position)
+#                 if not (worker is None):
+#                     await self.do(worker.build(ASSIMILATOR, vaspene))
+#
+# async def attack(self, iteration):
+#     self.log_info(iteration, "Attacking!")
+#     if (self.known_enemy_units.amount > 0):
+#         for s in self.units(PROBE):
+#             await self.do(s.attack(self.known_enemy_units.random))
+#
+# async def expand(self):
+#     if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+#         await self.expand_now()
 
 # ------------------------------------------------------
 
@@ -58,8 +60,7 @@ class MyBot(sc2.BotAI):
             await self.chat_send(f"I am a bot: {self.NAME}")
 
         if (iteration % self.LOGGING_FREQUENCY == 0):
-            await self.chat_send(f"Iteration: {iteration}")
-            await self.chat_send(f"Enemies detected: \
+            await self.chat_send(f"#{iteration} - Enemies detected: \
                                     {self.known_enemy_units.amount}")
 
 # ------------------------------------------------------
