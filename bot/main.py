0eba3fa725099cf8d88cfe23aa2041e25f8748e7
Alex Kazakov <kazakov_ab@mail.ru>
2018-07-21 21:34:03 +0200

    It can now train marines

--- bot/main.py
+++ bot/main.py
@@ -6,25 +6,29 @@ import sc2
 
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import COMMANDCENTER, SCV, SUPPLYDEPOT, ASSIMILATOR
+from sc2.constants import COMMANDCENTER, SUPPLYDEPOT, BARRACKS
+from sc2.constants import SCV, MARINE
 
 
 class MyBot(sc2.BotAI):
-    LOGGING_FREQUENCY = 50
+    LOGGING_FREQUENCY = 100
     NAME = load_bot_name()
 
     async def on_step(self, iteration):
+        self.iteration = iteration
         await self.print_intel_to_chat(iteration)
 
         await self.distribute_workers()  # in sc2/bot_ai.p
 
         await self.build_workers()
         await self.build_supply_depots()
+        await self.train_marines()
 
     async def build_workers(self):
         for command_center in self.units(COMMANDCENTER).ready.noqueue:
             if self.can_afford(SCV):
                 await self.do(command_center.train(SCV))
+                await self.chat_send(f"#{self.iteration} - Training SCV")
 
     async def build_supply_depots(self):
         if self.supply_left < 5 and not self.already_pending(SUPPLYDEPOT):
@@ -32,6 +36,13 @@ class MyBot(sc2.BotAI):
             if command_centers.exists:
                 if self.can_afford(SUPPLYDEPOT):
                     await self.build(SUPPLYDEPOT, near=command_centers.first)
+                    await self.chat_send(f"#{self.iteration} - Building Supply Depot")
+
+    async def train_marines(self):
+        for barrack in self.units(BARRACKS).ready.noqueue:
+            if self.can_afford(MARINE):
+                await self.do(barrack.train(MARINE))
+                await self.chat_send(f"#{self.iteration} - Training Marine")
 
 # async def build_assimilator(self):
 #     for nexus in self.units(NEXUS).ready:
@@ -59,8 +70,7 @@ class MyBot(sc2.BotAI):
         if iteration == 0:
             await self.chat_send(f"I am a bot: {self.NAME}")
 
-        if (iteration % self.LOGGING_FREQUENCY == 0):
-            await self.chat_send(f"#{iteration} - Enemies detected: \
-                                    {self.known_enemy_units.amount}")
+#       if (iteration % self.LOGGING_FREQUENCY == 0):
+#           await self.chat_send(f"#{iteration} - Enemies: {self.known_enemy_units.amount}")
 
 # ------------------------------------------------------
--- run_locally.py
+++ run_locally.py
@@ -15,7 +15,7 @@ def main():
 
     run_game(maps.get("(2)DreamcatcherLE"), [
         Bot(race, MyBot()),
-        Computer(Race.Terran, Difficulty.Easy)],
+        Computer(Race.Terran, Difficulty.Medium)],
         realtime=True, step_time_limit=2.0, game_time_limit=(60*40),
         save_replay_as="/Users/alexanderkazakov/Documents/MyCode/sc2-mailmanich-test-bot/test.SC2Replay")
 
