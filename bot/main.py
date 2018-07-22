4aa77baa37deb966ae0e482a7c3999f165008087
Alex Kazakov <kazakov_ab@mail.ru>
2018-07-22 21:26:51 +0200

    Added all the ideas to comments

--- bot/main.py
+++ bot/main.py
@@ -9,9 +9,34 @@ from sc2.player import Bot, Computer
 from sc2.constants import COMMANDCENTER, SUPPLYDEPOT, BARRACKS, FACTORY, STARPORT, ENGINEERINGBAY, ARMORY, REFINERY
 from sc2.constants import SCV, MARINE, SIEGETANK, MEDIVAC, VIKINGFIGHTER
 
+import math
+
 # Opportunities
 # - Build in a way that tanks can get out
+# - Try building turrets
+# - Get MULEs going
+# - Use stimpack on marines when in attack and there is a medivac?
 
+# Change so that it builds if no queue
+#  for factory in self.units(FACTORY).ready.noqueue:
+
+# Upgrading to orbital command
+# if self.units(UnitTypeId.BARRACKS).ready.exists and self.can_afford(UnitTypeId.BARRACKS): # we dont check if we can afford because the price for morphing units was/is bugged - doesn't work with "await self.do()"
+#            for cc in self.units(COMMANDCENTER).idle: # .idle filters idle command centers
+#                self.combinedActions.append(cc(AbilityId.UPGRADETOORBITAL_ORBITALCOMMAND))
+
+# Deploy MULEs
+# # manage orbital energy and drop mules
+# for oc in self.units(UnitTypeId.ORBITALCOMMAND).filter(lambda x: x.energy >= 50):
+#     mfs = self.state.mineral_field.closer_than(10, oc)
+#     if mfs:
+#         mf = max(mfs, key=lambda x:x.mineral_contents)
+#         self.combinedActions.append(oc(AbilityId.CALLDOWNMULE_CALLDOWNMULE, mf))
+#
+
+# HOW TO DEFINE BUILDING LOCATION
+#                         p = cc.position.towards_with_random_angle(self.game_info.map_center, 16)
+#                        await self.build(FACTORY, near=p)
 
 class MyBot(sc2.BotAI):
     MAX_SIMULATANEOUS_SUPPLY_DEPOTS_BUILDING = 2
@@ -21,39 +46,64 @@ class MyBot(sc2.BotAI):
     current_build_order_step = 0
     game_stage = 0
     last_iteration_with_order = 0
+    MAX_MEDIVACS = 3
     NAME = load_bot_name()
+
+# -----  HELPER FUNCTIONS ----------------------------------------
 
     def dependencies_satisfied(self, what_to_build):
         if what_to_build == BARRACKS:
             return self.units(SUPPLYDEPOT).ready.exists
-        if what_to_build == FACTORY:
+        elif what_to_build == FACTORY:
             return self.units(BARRACKS).ready.exists
-        if what_to_build == STARPORT:
+        elif what_to_build == STARPORT:
             return self.units(FACTORY).ready.exists
-        return True
+        else:
+            return True
 
     async def proceed_to_next_build_order_step(self):
-        await self.chat_send(f"#{self.iteration} - Build phase step {self.current_build_order_step} complete!")
+        await self.log(f"Build phase step {self.current_build_order_step} complete!")
         self.current_build_order_step += 1
         if self.current_build_order_step == len(self.INITIAL_BUILD_ORDER):
             self.game_stage += 1
-            await self.chat_send(f"#{self.iteration} - Build phase complete!")
+            await self.log("Build phase complete!")
 
     async def build_if_doesnt_exist(self, what_to_build):
-        if self.can_afford(what_to_build) and self.dependencies_satisfied(what_to_build) and (not self.already_pending(what_to_build)) and (not self.units(what_to_build).exists):
-            await self.build(what_to_build, near=self.units(COMMANDCENTER).first)
-            await self.chat_send(f"#{self.iteration} - Building {what_to_build.name}")
+        if (not self.already_pending(what_to_build)) and (not self.units(what_to_build).exists):
+            await self.build_structure_try(what_to_build)
+
+    async def initialize_step(self, iteration):
+        if iteration == 0:
+            await self.chat_send(f"I am a bot: {self.NAME}")
+
+        self.iteration = iteration
+        self.time = (self.state.game_loop/22.4)
+
+    def time_str(self):
+        minutes = math.floor(self.time / 60)
+        seconds = math.floor(self.time % 60)
+        return f"{minutes}:{str(seconds).zfill(2)}"
+
+    async def log(self, message):
+        await self.chat_send(f"#{self.time_str()} - {message}")
+
+    async def build_structure_try(self, structure):
+        if self.can_afford(structure) and self.dependencies_satisfied(structure) and self.units(COMMANDCENTER).ready.exists:
+            await self.build(structure, near=self.units(COMMANDCENTER).ready.first)
+            await self.log(f"Building {structure.name}")
+
+    async def train_unit_try(self, structure, unit):
+        if self.can_afford(unit):
+            await self.do(structure.train(unit))
+            await self.log(f"Training {unit.name}")
+
+
+# -----  MAIN LOOP ----------------------------------------
 
     async def on_step(self, iteration):
-        self.iteration = iteration
-        await self.print_intel_to_chat(iteration)
+        await self.initialize_step(iteration)
 
         await self.distribute_workers()  # in sc2/bot_ai.p
-
-        await self.build_workers()
-        await self.train_army()
-        await self.build_supply_depots()
-        await self.build_refinery()
 
         if self.game_stage == 0:
             # Program Expansion
@@ -63,7 +113,7 @@ class MyBot(sc2.BotAI):
             if (what_to_build == self.EXPAND):
                 if self.can_afford(COMMANDCENTER) and (not self.already_pending(COMMANDCENTER)) and (len(self.units(COMMANDCENTER)) < 2):
                     await self.expand_now()
-                    await self.chat_send(f"#{self.iteration} - Build phase - Expanding")
+                    await self.log("Build phase - Expanding")
                 if (len(self.units(COMMANDCENTER)) >= 2):
                     await self.proceed_to_next_build_order_step()
             else:  # Normal structure building here
@@ -72,25 +122,29 @@ class MyBot(sc2.BotAI):
                     await self.proceed_to_next_build_order_step()
         else:
             await self.expand()
-            await self.build_more_barracks()
+            await self.build_structures()
+
+        await self.build_workers()
+        await self.build_supply_depots()
+        await self.train_army()
+        await self.build_refinery()
+
+# -----  GAME MANAGEMENT ----------------------------------------
 
     async def build_workers(self):
         if self.units(SCV).amount < 70:
             for command_center in self.units(COMMANDCENTER).ready.noqueue:
-                if self.can_afford(SCV):
-                    await self.do(command_center.train(SCV))
-                    await self.chat_send(f"#{self.iteration} - Training SCV")
+                await self.train_unit_try(command_center, SCV)
 
     async def build_supply_depots(self):
-        if self.supply_left < self.SUPPLY_LEFT_TO_BUILD_DEPOT and self.already_pending(SUPPLYDEPOT) < self.MAX_SIMULATANEOUS_SUPPLY_DEPOTS_BUILDING and self.supply_cap < 200:
-            command_centers = self.units(COMMANDCENTER).ready
-            if command_centers.exists:
-                if self.can_afford(SUPPLYDEPOT):
-                    await self.build(SUPPLYDEPOT, near=command_centers.first)
-                    await self.chat_send(f"#{self.iteration} - Building Supply Depot")
+        if (self.supply_left < self.SUPPLY_LEFT_TO_BUILD_DEPOT and
+            self.already_pending(SUPPLYDEPOT) < self.MAX_SIMULATANEOUS_SUPPLY_DEPOTS_BUILDING and
+            self.supply_cap < 200):
+                await self.build_structure_try(SUPPLYDEPOT)
 
     async def build_refinery(self):
-        if (self.vespene < 100) and (not self.already_pending(REFINERY)) and self.can_afford(REFINERY):
+        if ((self.vespene < 100) and (not self.already_pending(REFINERY)) and
+            self.can_afford(REFINERY) and self.units(REFINERY).amount < math.floor(self.time/60)*2):
             for command_center in self.units(COMMANDCENTER).ready:
                 vaspenes = self.state.vespene_geyser.closer_than(10.0, command_center)
                 for vaspene in vaspenes:
@@ -106,65 +160,35 @@ class MyBot(sc2.BotAI):
         if self.supply_left > 1:
             for barrack in self.units(BARRACKS).ready:
                 if len(barrack.orders) < 2:
-                    if self.can_afford(MARINE):
-                        await self.do(barrack.train(MARINE))
-                        await self.chat_send(f"#{self.iteration} - Training Marine")
+                    await self.train_unit_try(barrack, MARINE)
 
             for factory in self.units(FACTORY).ready:
                 if len(factory.orders) < 1:
-                    if self.can_afford(SIEGETANK):
-                        await self.do(factory.train(SIEGETANK))
-                        await self.chat_send(f"#{self.iteration} - Producing a Tank")
+                    await self.train_unit_try(factory, SIEGETANK)
 
             for starport in self.units(STARPORT).ready:
                 if (len(starport.orders) < 1):
-                    if self.units(MEDIVAC).amount < 5:
-                        if self.can_afford(MEDIVAC):
-                            await self.do(starport.train(MEDIVAC))
-                            await self.chat_send(f"#{self.iteration} - Producing a Medivac")
+                    if self.units(MEDIVAC).amount < self.MAX_MEDIVACS:
+                        await self.train_unit_try(starport, MEDIVAC)
                     else:
-                        if self.can_afford(VIKINGFIGHTER):
-                            await self.do(starport.train(VIKINGFIGHTER))
-                            await self.chat_send(f"#{self.iteration} - Producing a Viking")
+                        await self.train_unit_try(starport, VIKINGFIGHTER)
 
     async def expand(self):
-        if self.units(COMMANDCENTER).amount < 4 and self.can_afford(COMMANDCENTER):
+        if (self.units(COMMANDCENTER).amount < 4 and
+                    self.can_afford(COMMANDCENTER) and
+                    self.units(SCV).amount > self.units(COMMANDCENTER).amount*16):
             await self.expand_now()
-            await self.chat_send(f"#{self.iteration} - Expanding to new location")
+            await self.log("Expanding to new location")
 
-    async def build_more_barracks(self):
-        if self.supply_left > 10 and not self.already_pending(BARRACKS) and self.can_afford(BARRACKS) and self.units(BARRACKS).amount < self.units(COMMANDCENTER).amount*2:
-            command_centers = self.units(COMMANDCENTER).ready
-            if command_centers.exists:
-                await self.build(BARRACKS, near=command_centers.random)
-                await self.chat_send(f"#{self.iteration} - Building Barracks")
+    async def build_structures(self):
+        await self.build_more_barracks()
 
         await self.build_if_doesnt_exist(FACTORY)
         await self.build_if_doesnt_exist(STARPORT)
         await self.build_if_doesnt_exist(ENGINEERINGBAY)
-        await self.build_if_doesnt_exist(ARMORY)
+#        await self.build_if_doesnt_exist(ARMORY)
 
-
-# async def build_assimilator(self):
-#     for nexus in self.units(NEXUS).ready:
-#         vaspenes = self.state.vespene_geyser.closer_than(10.0, nexus)
-#         for vaspene in vaspenes:
-#             if (not self.units(ASSIMILATOR).closer_than(1.0, vaspene)
-#                               .exists) and self.can_afford(ASSIMILATOR):
-#                 worker = self.select_build_worker(vaspene.position)
-#                 if not (worker is None):
-#                     await self.do(worker.build(ASSIMILATOR, vaspene))
-#
-# async def attack(self, iteration):
-#     self.log_info(iteration, "Attacking!")
-#     if (self.known_enemy_units.amount > 0):
-#         for s in self.units(PROBE):
-#             await self.do(s.attack(self.known_enemy_units.random))
-
-# ------------------------------------------------------
-
-    async def print_intel_to_chat(self, iteration):
-        if iteration == 0:
-            await self.chat_send(f"I am a bot: {self.NAME}")
-
-# ------------------------------------------------------
+    async def build_more_barracks(self):
+        if (self.supply_left > 5 and not self.already_pending(BARRACKS) and
+                self.units(BARRACKS).amount < self.units(COMMANDCENTER).amount*2):
+            await self.build_structure_try(BARRACKS)
